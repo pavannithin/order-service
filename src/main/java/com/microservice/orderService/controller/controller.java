0740113ac8145335bb70b3c1044ec81f64ae5462
pavannithin <nithin.pavan@gmail.com>
2024-07-11 01:20:55 +0530

    error fix

--- src/main/java/com/microservice/orderService/controller/controller.java
+++ src/main/java/com/microservice/orderService/controller/controller.java
@@ -3,7 +3,6 @@ package com.microservice.orderService.controller;
 import com.microservice.orderService.dto.OrderRequest;
 import com.microservice.orderService.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
